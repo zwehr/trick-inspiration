5162aa7e075a14fb5aee4ebffdca2b447a6035ea
zwehr <zwehrmeister@gmail.com>
2023-06-24 17:03:27 -0400

    create basic tricks api route

---
+++ src/pages/api/tricks.ts
@@ -0,0 +1,19 @@
+import clientPromise from '../../lib/mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db('video');
+
+    const movies = await db
+      .collection('tricks-webm')
+      .find({})
+      .limit(10)
+      .toArray();
+
+    res.json(movies);
+  } catch (e) {
+    console.error(e);
+  }
+};
